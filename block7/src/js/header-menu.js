34a29ecce3fcb3ce9916af7091835d5c956edbf2
Zakhar Kibanov <z.kibanov@yandex.ru>
2020-10-20 14:38:26 +0300

    Problem test

---
+++ block7/src/js/header-menu.js
@@ -0,0 +1,9 @@
+const header__menubtn = document.querySelector('.header__menu-btn');
+const burger__menu = document.querySelector('.sidebar-menu-body');
+const button__close = document.querySelector('.button--close');
+header__menubtn.addEventListener('click', function (){
+burger__menu.classList.toggle('sidebar-menu-body--hidden');
+});
+button__close.addEventListener('click',function(){
+burger__menu.classList.toggle('sidebar-menu-body--hidden');
+});
---
+++ block7/src/js/swiper1.js
@@ -0,0 +1,51 @@
+import Swiper, { Pagination } from 'swiper';
+import 'swiper/swiper-bundle.css';
+
+const btnMore = document.querySelector('.brands__btn-more');
+let minimizer = document.querySelector('.brands-list_minimizer');
+let mySwiper;
+const mediaQueryList = window.matchMedia('(min-width: 768px)');
+
+btnMore.addEventListener('click', function () {
+    btnMore.classList.toggle('btn-more_opened');
+    minimizer.classList.toggle('brands-list_minimized');
+
+    if (minimizer.classList.contains('brands-list_minimized')) {
+        btnMore.textContent = 'Показать все';
+    } else {
+        btnMore.textContent = 'Скрыть';
+    }
+})
+
+const enableSwiper = function() {
+    mySwiper = new Swiper('.swiper-container', {
+        slidesPerView: 2,
+        spaceBetween: 16,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+    });
+}
+
+let swiperRequest = function (a) {
+    if (( a.matches ) && (mySwiper!==undefined)) {
+        mySwiper.destroy(true, true)
+    } else if (a.matches) {
+        console.log(mySwiper)
+        return ;
+    } else {
+        enableSwiper();
+    }
+}
+
+swiperRequest(mediaQueryList);
+
+mediaQueryList.onchange = function (a) {
+    swiperRequest(a);
+}
+
+
+
+
