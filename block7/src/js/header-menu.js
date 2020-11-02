a86f72bf8254ada18f122f4d57227192edcb9edb
Zakhar Kibanov <z.kibanov@yandex.ru>
2020-11-02 13:10:03 +0300

    menu closes @ 768+

--- block7/src/js/header-menu.js
+++ block7/src/js/header-menu.js
@@ -1,30 +1,24 @@
 const header__menubtn = document.querySelector(".header__menu-btn");
 const burger__menu = document.querySelector(".sidebar-menu-body");
 const button__close = document.querySelector(".sidebar-menu-btn");
-const sidebar__menu__blur = document.querySelector(".sidebar-menu--blur");
+const background = document.querySelector(".sidebar-menu--background");
 let body = document.querySelector(".body");
-
+const closeSidebar = function () {
+  burger__menu.classList.add("sidebar-menu-body--hidden");
+  body.classList.remove("blocked");
+  background.classList.remove("visible");
+  setTimeout(background.addEventListener("click",function(){
+    closeSidebar();
+  }), 800);
+};
 
 header__menubtn.addEventListener("click", function () {
   burger__menu.classList.remove("sidebar-menu-body--hidden");
   body.classList.add("blocked");
+  background.classList.add("visible");
 });
 
 button__close.addEventListener("click", function () {
-  burger__menu.classList.toggle("sidebar-menu-body--hidden");
-  body.classList.remove("blocked");
+  closeSidebar();
 });
 
-const closeSidebar = function () {
-  let sidebar__menu__blurVisible;
-  sidebar__menu__blurVisible = document.querySelector(".visible");
-  if (sidebar__menu__blurVisible !== null) {
-    sidebar__menu__blurVisible.addEventListener("click", function () {
-      if (sidebar__menu__blur.classList.contains("visible")) {
-        sidebar__menu__blur.classList.remove("visible");
-        console.log(sidebar__menu__blurVisible);
-        burger__menu.classList.add("sidebar-menu-body--hidden");
-      }
-    });
-  }
-};
